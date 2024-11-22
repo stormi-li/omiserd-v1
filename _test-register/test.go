1ae8d88ffdbe3c14a1f851711e897d0b8e057af2
Your Name <your.email@example.com>
2024-11-22 21:03:27 +0800

    重构了代码

--- _test-register/test.go
+++ _test-register/test.go
@@ -1,18 +1,31 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/go-redis/redis/v8"
 	"github.com/stormi-li/omiserd-v1"
+	omiconst "github.com/stormi-li/omiserd-v1/omiserd_const"
+	register "github.com/stormi-li/omiserd-v1/omiserd_register"
 )
 
 var redisAddr = "118.25.196.166:3934"
 var password = "12982397StrongPassw0rd"
 
 func main() {
-	omiC := omiserd.NewClient(&redis.Options{Addr: redisAddr, Password: password}, omiserd.Web)
+	omiC := omiserd.NewClient(&redis.Options{Addr: redisAddr, Password: password}, omiconst.Web)
 	// r := omiC.NewRegister("web_service", "localhost:8080")
 	// r.RegisterAndServe(1, func(port string) {})
 	r := omiC.NewRegister("web_service", "localhost:8081")
+	r.AddMessageHandleFunc(func(command, message string, register *register.Register) {
+		fmt.Println(command, message)
+	})
+	count := 0
+	r.AddRegisterHandleFunc(func(register *register.Register) {
+		register.Data["count"] = strconv.Itoa(count)
+		count++
+	})
 	r.RegisterAndServe(1, func(port string) {})
 	select {}
 }
