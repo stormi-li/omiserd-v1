0a79f9b113fdd9fc88f5cffe85e5a18cd62f86dd
Your Name <your.email@example.com>
2024-11-26 23:18:23 +0800

    refine monitor

--- omiserd_Monitor.go
+++ omiserd_Monitor.go
@@ -3,14 +3,15 @@ package omiserd
 import "time"
 
 type Monitor struct {
-	serverName string
-	discover   *Discover
+	ServerName string
+	Address    string
+	Discover   *Discover
 }
 
 func NewMonitor(serverName string, d *Discover) *Monitor {
 	return &Monitor{
-		serverName: serverName,
-		discover:   d,
+		ServerName: serverName,
+		Discover:   d,
 	}
 }
 
@@ -19,12 +20,11 @@ func NewMonitor(serverName string, d *Discover) *Monitor {
 // - listenFunc: 监听逻辑，返回新的地址（或空字符串表示无变化）
 // - connectFunc: 连接逻辑，处理新的地址
 func (monitor *Monitor) ListenAndConnect(interval time.Duration, listenFunc func(serverName, oldAddress string, discover *Discover) string, connectFunc func(address string)) {
-	oldAddress := ""
 	for {
-		address := listenFunc(monitor.serverName, oldAddress, monitor.discover)
+		address := listenFunc(monitor.ServerName, monitor.Address, monitor.Discover)
 		if address != "" {
 			connectFunc(address)
-			oldAddress = address
+			monitor.Address = address
 		}
 		time.Sleep(interval)
 	}
